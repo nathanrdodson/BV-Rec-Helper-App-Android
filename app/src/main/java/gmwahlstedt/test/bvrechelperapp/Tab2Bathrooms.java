e066c645e0cdfc8cf231aa4f9d1a1aad91533d7b
LimeLava <Garrett@mrwnet.com>
2018-11-01 09:12:33 -0500

    Tabs Working

---
+++ app/src/main/java/gmwahlstedt/test/bvrechelperapp/Tab2Bathrooms.java
@@ -0,0 +1,19 @@
+package gmwahlstedt.test.bvrechelperapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2Bathrooms extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2bathrooms, container, false);
+        return rootView;
+    }
+
+}
+
