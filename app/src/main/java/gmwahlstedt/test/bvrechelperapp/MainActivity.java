1ee567eddd863b0f32429b9a1651458f3623c3d5
cggianni <43577984+cggianni@users.noreply.github.com>
2018-11-15 09:11:38 -0600

    Fix this

--- app/src/main/java/gmwahlstedt/test/bvrechelperapp/MainActivity.java
+++ app/src/main/java/gmwahlstedt/test/bvrechelperapp/MainActivity.java
@@ -12,13 +12,9 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 
 import android.widget.TextView;
 
--- app/src/main/java/gmwahlstedt/test/bvrechelperapp/NonSwipeableViewPager.java
+++ app/src/main/java/gmwahlstedt/test/bvrechelperapp/NonSwipeableViewPager.java
@@ -9,49 +9,22 @@ import java.lang.reflect.Field;
 
 public class NonSwipeableViewPager extends ViewPager {
 
+    private boolean canScroll = false;
     public NonSwipeableViewPager(Context context) {
         super(context);
-        setMyScroller();
     }
-
     public NonSwipeableViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setMyScroller();
     }
-
+    public void setCanScroll(boolean canScroll) {
+        this.canScroll = canScroll;
+    }
     @Override
-    public boolean onInterceptTouchEvent(MotionEvent event) {
-        // Never allow swiping to switch between pages
-        return false;
+    public boolean onTouchEvent(MotionEvent ev) {
+        return canScroll && super.onTouchEvent(ev);
     }
-
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        // Never allow swiping to switch between pages
-        return false;
-    }
-
-    //down one is added for smooth scrolling
-
-    private void setMyScroller() {
-        try {
-            Class<?> viewpager = ViewPager.class;
-            Field scroller = viewpager.getDeclaredField("mScroller");
-            scroller.setAccessible(false);
-            scroller.set(this, new MyScroller(getContext()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public class MyScroller extends Scroller {
-        public MyScroller(Context context) {
-            super(context, new DecelerateInterpolator());
-        }
-
-        @Override
-        public void startScroll(int startX, int startY, int dx, int dy, int duration) {
-            super.startScroll(startX, startY, dx, dy, 350 /*1 secs*/);
-        }
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return canScroll && super.onInterceptTouchEvent(ev);
     }
 }
